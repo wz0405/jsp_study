98c8983408119f335b06264bdc6e70b791c41452
hyunheehan <hyunheehan@172.30.1.7>
2020-06-17 17:53:41 +0900

    0617_DAO_DTO

---
+++ JspProject/WebContent/Info/InfoList.jsp
@@ -0,0 +1,47 @@
+<%@page import="info.model.infoDTO"%>
+<%@page import="java.util.Vector"%>
+<%@page import="info.model.infoDAO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script src="../jquery-3.5.1.min.js"></script>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+</head>
+<body>
+	<%
+		infoDAO dao = new infoDAO();
+	Vector<infoDTO> list = dao.getAllDatas();
+	%>
+	<a href="InfoForm.jsp">데이타추가</a>
+	<hr>
+	<table class="table table-bordered">
+		<caption>info리스트 출력</caption>
+		<tr>
+			<th width="50">번호</th>
+			<th width="70">이름</th>
+			<th width="150">주소</th>
+			<th width="150">현재날짜</th>
+			<th>수정/삭제</th>
+		</tr>
+	<%
+	for(int i=0;i<list.size();i++){
+		infoDTO dto = list.get(i);%>
+		<tr>
+			<td><%=i+1%></td>
+			<td><%=dto.getName()%></td>
+			<td><%=dto.getAddr()%></td>
+			<td><%=dto.getSdate()%></td>
+			<td><a>수정</a><a>삭제</a></td>
+		</tr>
+	<%
+	}
+	%>
+	</table>
+</body>
+</html>
---
+++ JspProject/src/info/model/infoDAO.java
@@ -0,0 +1,112 @@
+package info.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+
+
+public class infoDAO { //명령전송기능 
+	String driver="com.mysql.jdbc.Driver";
+	String url="jdbc:mysql://localhost:3306/root";
+	public infoDAO() {
+		
+		try {
+			Class.forName(driver);
+			System.out.println("오라클드라이버 성공");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("오라클드라이버 실패");
+			
+		}
+	}
+	public Connection getConnection()
+	{
+		Connection conn=null;
+		try {
+			conn=DriverManager.getConnection(url,"root","1234");
+			System.out.println("Db 연동 성공");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("DB 연동 실패");
+		}
+		return conn;
+	}
+	//insert
+	public void infoInsert(infoDTO dto) {
+		
+		
+		String sql="insert into info values(null,?,?,now())";
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		
+		conn=getConnection();
+		try {
+			pstmt=conn.prepareStatement(sql);
+			//??에 대한 바인딩
+			pstmt.setString(1, dto.getName());
+			pstmt.setString(2, dto.getAddr());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("insert fail");
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	///////
+	public Vector<infoDTO> getAllDatas(){
+		Vector<infoDTO> list = new Vector<infoDTO>();
+		Connection conn =null;
+		PreparedStatement pstmt=null;
+		ResultSet rs= null;
+		String sql="select * from info order by num asc";
+		conn=getConnection();
+		try {
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			//여러개의 데이터를 얻는 경우 while
+			while(rs.next()) {
+				//반드시 while 문 안에서 선
+				infoDTO dto=new infoDTO();
+				dto.setNum(rs.getString("num"));
+				dto.setName(rs.getString("name"));
+				dto.setAddr(rs.getString("addr"));
+				dto.setSdate(rs.getTimestamp("sdate"));
+				list.add(dto);
+			}
+			//벡터에 추가
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		return list;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+
+}
